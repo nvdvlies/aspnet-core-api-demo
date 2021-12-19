63f5111690988b7b17c2d878e6b2962f191037b1
Niels van der Vlies <nvandervlies@snelstart.nl>
2021-12-19 12:59:58 +0100

    extra logging

--- src/Demo.WebApi/Helpers/DatabaseMigrationHelper.cs
+++ src/Demo.WebApi/Helpers/DatabaseMigrationHelper.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Linq;
 
 namespace Demo.WebApi.Helpers
 {
@@ -11,25 +12,41 @@ namespace Demo.WebApi.Helpers
     {
         public static IHost MigrateDatabase(this IHost host)
         {
-            using (var scope = host.Services.CreateScope())
+            using var scope = host.Services.CreateScope();
+            using var appContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            var logger = scope.ServiceProvider.GetRequiredService<ILoggerFactory>().CreateLogger(nameof(DatabaseMigrationHelper));
+            try
             {
-                using (var appContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>())
+                logger.LogInformation("Retrieving pending database migrations.");
+
+                var pendingMigrations = appContext.Database.GetPendingMigrations();
+
+                if (pendingMigrations.Any())
                 {
-                    var logger = scope.ServiceProvider.GetRequiredService<ILoggerFactory>().CreateLogger(nameof(DatabaseMigrationHelper));
-                    try
-                    {
-                        logger.LogInformation("Starting database migration");
-                        appContext.Database.Migrate();
-                        logger.LogInformation("Finished database migration");
-                    }
-                    catch (Exception ex)
-                    {
-                        logger.LogError(ex, "Database migration failed with error: {0}", ex.Message);
-                        throw;
-                    }
+                    logger.LogInformation("Pending database migrations are available.");
+
+                    var lastAppliedMigration = appContext.Database.GetAppliedMigrations().Last();
+
+                    logger.LogInformation("Database currently on migration '{lastAppliedMigration}'.", lastAppliedMigration);
+                    logger.LogInformation("Applying {count} migration(s) ({names}).", pendingMigrations.Count(), string.Join(",", pendingMigrations));
+
+                    appContext.Database.Migrate();
+
+                    lastAppliedMigration = appContext.Database.GetAppliedMigrations().Last();
+
+                    logger.LogInformation("Database migrated to '{lastAppliedMigration}'.", lastAppliedMigration);
+                }
+                else
+                {
+                    logger.LogInformation("No pending database migrations available.");
                 }
             }
-
+            catch (Exception ex)
+            {
+                logger.LogCritical(ex, "Database migration failed with error: {message}.", ex.Message);
+                throw;
+            }
+            
             return host;
         }
     }
