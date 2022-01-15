2379716cb6f083d8aa20397d9df379f953d7d79b
Niels van der Vlies <nvandervlies@snelstart.nl>
2022-01-15 11:34:53 +0100

    ng g module features/invoices --module app --r...

--- src/Demo.WebUI/src/app/app-routing.module.ts
+++ src/Demo.WebUI/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'customers', loadChildren: () => import('./features/customers/customers.module').then(m => m.CustomersModule) }];
+const routes: Routes = [
+  { 
+    path: 'customers', 
+    loadChildren: () => import('./features/customers/customers.module').then(m => m.CustomersModule) 
+  }, 
+  { 
+    path: 'invoices', 
+    loadChildren: () => import('./features/invoices/invoices.module').then(m => m.InvoicesModule) 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
