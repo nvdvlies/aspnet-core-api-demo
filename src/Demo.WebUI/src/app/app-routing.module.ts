0e36d5dfe5063791cf6d9cdb17d05c15389b6d29
Niels van der Vlies <nvandervlies@snelstart.nl>
2022-01-15 11:45:31 +0100

    added canActivate guards

--- src/Demo.WebUI/src/app/app-routing.module.ts
+++ src/Demo.WebUI/src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '@auth0/auth0-angular';
 
 const routes: Routes = [
   { 
     path: 'customers', 
-    loadChildren: () => import('./features/customers/customers.module').then(m => m.CustomersModule) 
+    loadChildren: () => import('./features/customers/customers.module').then(m => m.CustomersModule),
+    canActivate: [AuthGuard]
   }, 
   { 
     path: 'invoices', 
-    loadChildren: () => import('./features/invoices/invoices.module').then(m => m.InvoicesModule) 
+    loadChildren: () => import('./features/invoices/invoices.module').then(m => m.InvoicesModule),
+    canActivate: [AuthGuard]
   }
 ];
 
