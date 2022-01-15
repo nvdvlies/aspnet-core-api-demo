213fba3cf9de231699a83c4ba50492818554e40e
Niels van der Vlies <nvandervlies@snelstart.nl>
2022-01-15 11:27:36 +0100

    core module forRoot

--- src/Demo.WebUI/src/app/app.module.ts
+++ src/Demo.WebUI/src/app/app.module.ts
@@ -14,7 +14,7 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    CoreModule
+    CoreModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/Demo.WebUI/src/app/core/core.module.ts
+++ src/Demo.WebUI/src/app/core/core.module.ts
@@ -1,7 +1,5 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NgModule, Optional, SkipSelf, ModuleWithProviders } from '@angular/core';
 
 @NgModule({
   declarations: [],
@@ -9,4 +7,18 @@ import { CommonModule } from '@angular/common';
     CommonModule
   ]
 })
-export class CoreModule { }
+export class CoreModule {
+  static forRoot(): ModuleWithProviders<CoreModule> {
+    return {
+        ngModule: CoreModule,
+        providers: [
+        ]
+    };
+  }
+
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+      if (parentModule) {
+          throw new Error("CoreModule is already loaded. Import in AppModule only");
+      }
+  }
+}
