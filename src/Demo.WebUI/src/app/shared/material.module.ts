89f9b7f321521a727e7e6e2010b08a5db2a349e3
Niels van der Vlies <nvandervlies@snelstart.nl>
2022-01-15 11:30:19 +0100

    export material components

--- src/Demo.WebUI/src/app/shared/material.module.ts
+++ src/Demo.WebUI/src/app/shared/material.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
+  exports: [
+    MatButtonModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatMenuModule
   ]
 })
 export class MaterialModule { }
