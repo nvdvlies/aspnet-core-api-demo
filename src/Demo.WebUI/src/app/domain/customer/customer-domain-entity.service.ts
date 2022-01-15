0815d68f7c5ef3d1b432372f73fa4047ccebbbcd
Niels van der Vlies <nvandervlies@snelstart.nl>
2022-01-15 12:10:04 +0100

    Added CustomerDomainEntity

---
+++ src/Demo.WebUI/src/app/domain/customer/customer-domain-entity.service.ts
@@ -0,0 +1,115 @@
+import { Injectable, OnDestroy } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { combineLatest, Observable } from 'rxjs';
+import { debounceTime, map } from 'rxjs/operators';
+import { CustomerDto, ICustomerDto } from '@api/api.generated.clients';
+import { CustomerStoreService } from '@domain/customer/customer-store.service';
+import { DomainEntityBase, DomainEntityContext, IDomainEntityContext, InitFromRouteOptions } from '@domain/shared/domain-entity-base';
+
+export interface ICustomerDomainEntityContext extends IDomainEntityContext<CustomerDto> {
+
+}
+
+export class CustomerDomainEntityContext extends DomainEntityContext<CustomerDto> implements ICustomerDomainEntityContext {
+  constructor() {
+    super();
+  }
+}
+
+type CustomerControls = { [key in keyof ICustomerDto]: AbstractControl };
+export type CustomerFormGroup = FormGroup & { controls: CustomerControls };  
+
+@Injectable()
+export class CustomerDomainEntityService extends DomainEntityBase<CustomerDto> implements OnDestroy {
+  protected getByIdFunction = (id: string) => this.customerStoreService.getById(id);
+  protected createFunction = (customer: CustomerDto) => this.customerStoreService.create(customer);
+  protected updateFunction = (customer: CustomerDto) => this.customerStoreService.update(customer);
+  protected deleteFunction = (id: string) => this.customerStoreService.delete(id);
+  protected entityUpdatedEvent$ = this.customerStoreService.customerUpdatedInStore$;
+
+  public observe$ = combineLatest([
+    this.observeInternal$
+  ])
+    .pipe(
+      debounceTime(0),
+      map(([
+        context,
+      ]) => {
+        return {
+          ...context
+        } as CustomerDomainEntityContext;
+      })
+    ) as Observable<CustomerDomainEntityContext>;
+
+  constructor(
+    route: ActivatedRoute,
+    protected readonly formBuilder: FormBuilder,
+    protected readonly customerStoreService: CustomerStoreService
+  ) {
+    super(route);
+    super.init();
+  }
+
+  public override get form(): CustomerFormGroup {
+    return super.form as CustomerFormGroup;
+  }
+  public override set form(value: CustomerFormGroup) {
+    super.form = value;
+  }
+
+  public override new(): Observable<null> {
+    return super.new();
+  }
+
+  public override getById(id: string): Observable<null> {
+    return super.getById(id);
+  }
+
+  public override initFromRoute(options?: InitFromRouteOptions | undefined): Observable<null> {
+    return super.initFromRoute(options);
+  }
+
+  public override create(): Observable<CustomerDto> {
+    return super.create();
+  }
+
+  public override update(): Observable<CustomerDto> {
+    return super.update();
+  }
+
+  public override upsert(): Observable<CustomerDto> {
+    return super.upsert();
+  }
+
+  public override delete(): Observable<void> {
+    return super.delete();
+  }
+
+  protected instantiateForm(): void {
+    this.form = this.buildInvoiceFormGroup();
+  }
+
+  private buildInvoiceFormGroup(): CustomerFormGroup {
+    return new FormGroup({
+      id: new FormControl(),
+      name: new FormControl('', [Validators.required, Validators.minLength(2)], []),
+      deleted: new FormControl(),
+      deletedBy: new FormControl(),
+      deletedOn: new FormControl(),
+      createdBy: new FormControl(),
+      createdOn: new FormControl(),
+      lastModifiedBy: new FormControl(),
+      lastModifiedOn: new FormControl(),
+      timestamp: new FormControl()
+    } as CustomerControls) as CustomerFormGroup;
+  }
+
+  protected instantiateNewEntity(): CustomerDto {
+    return new CustomerDto();
+  }
+
+  public override reset(): void {
+    super.reset()
+  }
+}
