b6ab993639e5d3dc27973df041f4a2c351395b3a
Niels van der Vlies <nvandervlies@snelstart.nl>
2022-01-15 11:31:20 +0100

    ng g module domain --module app

--- src/Demo.WebUI/src/app/app.module.ts
+++ src/Demo.WebUI/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { DomainModule } from './domain/domain.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { SharedModule } from './shared/shared.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     CoreModule.forRoot(),
-    SharedModule
+    SharedModule,
+    DomainModule
   ],
   providers: [],
   bootstrap: [AppComponent]
