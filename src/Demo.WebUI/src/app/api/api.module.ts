0c80e8cae4f1d0cd71c03e714ee375d83265cd33
Niels van der Vlies <nvandervlies@snelstart.nl>
2022-01-15 11:46:09 +0100

    added AuthHttpInterceptor

--- src/Demo.WebUI/src/app/api/api.module.ts
+++ src/Demo.WebUI/src/app/api/api.module.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule, Optional, SkipSelf, ModuleWithProviders } from '@angular/core';
 import { API_BASE_URL } from '@api/api.generated.clients';
 import { SIGNALR_BASE_URL } from '@api/signalr.generated.services';
+import { AuthHttpInterceptor } from '@auth0/auth0-angular';
 import { environment } from '@env/environment';
 
 @NgModule({
@@ -15,6 +17,11 @@ export class ApiModule {
     return {
         ngModule: ApiModule,
         providers: [
+          {
+            provide: HTTP_INTERCEPTORS,
+            useClass: AuthHttpInterceptor,
+            multi: true,
+          },
           { provide: API_BASE_URL, useValue: environment.apiBaseUrl },
           { provide: SIGNALR_BASE_URL, useValue: environment.apiBaseUrl }
         ]
