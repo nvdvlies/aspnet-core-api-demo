a6b7f2de56f4978551b0f1fe304194f2713b36f5
Niels van der Vlies <nvandervlies@snelstart.nl>
2022-01-15 11:47:19 +0100

    Added login redirect

--- src/Demo.WebUI/src/app/app.component.ts
+++ src/Demo.WebUI/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Demo.WebUI';
+export class AppComponent implements OnInit {
+  constructor(
+    public authService: AuthService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.authService.isAuthenticated$.subscribe(isAuthenticated => {
+      if (!isAuthenticated) {
+        this.authService.loginWithRedirect();
+      }
+    });
+  }
 }
