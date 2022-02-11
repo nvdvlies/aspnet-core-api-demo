1041731cb4df5f39ffbe9a2ecde80dd138f4f8c3
Niels van der Vlies <nvandervlies@snelstart.nl>
2022-02-11 11:48:19 +0100

    Use auth0 user id

--- src/Demo.WebApi/Services/CurrentUserService.cs
+++ src/Demo.WebApi/Services/CurrentUserService.cs
@@ -1,12 +1,22 @@
 ﻿using Demo.Common.Interfaces;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Globalization;
+using System.Linq;
+using System.Security.Claims;
 
 namespace Demo.WebApi.Services
 {
     public class CurrentUserService : ICurrentUser
     {
-        public Guid Id => Guid.NewGuid();
+        private readonly IHttpContextAccessor _context;
+
+        public CurrentUserService(IHttpContextAccessor context)
+        {
+            _context = context;
+        }
+
+        public Guid Id => Guid.NewGuid(); // _context.HttpContext.User?.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value;
 
         public TimeZoneInfo TimeZone => TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time"); // linux: Europe/Amsterdam
 
