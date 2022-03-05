f7ca887b50e63284e71ba6e75caba1ca1fbd4dd4
Niels van der Vlies <nvandervlies@snelstart.nl>
2022-03-05 13:35:41 +0100

    added caching to lookup service

--- src/Demo.WebUI/src/app/shared/services/customer-lookup.service.ts
+++ src/Demo.WebUI/src/app/shared/services/customer-lookup.service.ts
@@ -4,17 +4,43 @@ import {
   CustomerLookupDto,
   CustomerLookupOrderByEnum
 } from '@api/api.generated.clients';
-import { map, Observable } from 'rxjs';
+import { BehaviorSubject, map, Observable, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerLookupService {
+  private readonly cache = new BehaviorSubject<CustomerLookupDto[]>([]);
+
+  private get items(): CustomerLookupDto[] {
+    return this.cache.getValue();
+  }
+
+  private set items(value: CustomerLookupDto[]) {
+    this.cache.next(value);
+  }
+
+  private addToCache(item: CustomerLookupDto): void {
+    this.items = [...this.items, item];
+  }
+
   constructor(private readonly apiCustomersClient: ApiCustomersClient) {}
 
   public getById(id: string): Observable<CustomerLookupDto | undefined> {
-    return this.apiCustomersClient
-      .lookup(CustomerLookupOrderByEnum.Name, false, 0, 1, undefined, [id])
-      .pipe(map((x) => x.customers?.[0]));
+    const cachedItem = this.cache.value.find((x) => x.id?.toLowerCase() === id.toLowerCase());
+    if (cachedItem) {
+      return of(cachedItem);
+    } else {
+      return this.apiCustomersClient
+        .lookup(CustomerLookupOrderByEnum.Name, false, 0, 1, undefined, [id])
+        .pipe(
+          map((response) => response.customers?.[0]),
+          tap((customer) => {
+            if (customer) {
+              this.addToCache(customer);
+            }
+          })
+        );
+    }
   }
 }
