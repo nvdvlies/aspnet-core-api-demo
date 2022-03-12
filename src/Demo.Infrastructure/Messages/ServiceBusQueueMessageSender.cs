1a2b43719076d86b76d5d55abeae2765b05d0475
Niels van der Vlies <nvandervlies@snelstart.nl>
2022-03-12 13:06:28 +0100

    Disable service bus queue sender in debug

--- src/Demo.Infrastructure/Messages/ServiceBusQueueMessageSender.cs
+++ src/Demo.Infrastructure/Messages/ServiceBusQueueMessageSender.cs
@@ -14,14 +14,20 @@ namespace Demo.Infrastructure.Messages
 
         public ServiceBusQueueMessageSender(EnvironmentSettings environmentSettings)
         {
+#if !DEBUG
             _client = new ServiceBusClient(environmentSettings.ServiceBus.Namespace, new DefaultAzureCredential());
+#endif
         }
 
         public async Task SendAsync(IMessage message, CancellationToken cancellationToken)
         {
             var serviceBusMessage = new ServiceBusMessage(JsonSerializer.Serialize(message));
+#if DEBUG
+            await Task.CompletedTask;
+#else
             var sender = _client.CreateSender(message.Queue.ToString());
             await sender.SendMessageAsync(serviceBusMessage, cancellationToken);
+#endif
         }
     }
 }
