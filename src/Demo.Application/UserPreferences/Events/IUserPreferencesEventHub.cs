ed0479822ab7d806652d53d6072c0bdd33d3e0a2
Niels van der Vlies <nvlies@hotmail.com>
2022-04-23 14:14:10 +0200

    Removed UserPreferencesCreated event

--- src/Demo.Application/UserPreferences/Events/IUserPreferencesEventHub.cs
+++ src/Demo.Application/UserPreferences/Events/IUserPreferencesEventHub.cs
@@ -5,7 +5,6 @@ namespace Demo.Application.UserPreferences.Events
 {
     public interface IUserPreferencesEventHub
     {
-        Task UserPreferencesCreated(Guid id, Guid createdBy);
         Task UserPreferencesUpdated(Guid id, Guid updatedBy);
         Task UserPreferencesDeleted(Guid id, Guid deletedBy);
         // SCAFFOLD-MARKER: EVENTHUB
