09c394c1d49c62afaaabb75375dd3920eab59612
Niels van der Vlies <nvlies@hotmail.com>
2022-04-23 10:20:56 +0200

    Added  generate-api-clients command to package.json

--- tools/nswag/nswag.ps1
+++ tools/nswag/nswag.ps1
@@ -1,5 +1,9 @@
+$MyInvocation.MyCommand.Path | Split-Path | Push-Location
+
 Write-Host 'Installing Nswag...'
 npm i -g nswag@13.15.5
 
 Write-Host 'Generating api.service.ts...'
 nswag run nswag.json /runtime:Net50
+
+Pop-Location
