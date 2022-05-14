b46f21a22e8a1eff1548e1e55c94712486df4820
Niels van der Vlies <nvlies@hotmail.com>
2022-05-14 11:19:22 +0200

    Revert "fix select input type=number"
    This reverts commit b796fa3ef2d09bf421988c8be00ba5b167f6c909.

--- src/Demo.WebUI/src/app/shared/directives/select-on-focus.directive.ts
+++ src/Demo.WebUI/src/app/shared/directives/select-on-focus.directive.ts
@@ -2,7 +2,7 @@ import { Directive, HostListener, Inject, ElementRef } from '@angular/core';
 
 @Directive({
   selector:
-    'input[type=text]:not(.disable-select-on-focus), input[type=number]:not(.disable-select-on-focus), input[type=email]:not(.disable-select-on-focus)'
+    'input[type=text]:not(.disable-select-on-focus), input[type=email]:not(.disable-select-on-focus)'
 })
 export class SelectOnFocusDirective {
   constructor(@Inject(ElementRef) private elementRef: ElementRef) {}
@@ -11,7 +11,7 @@ export class SelectOnFocusDirective {
   public onFocus(): void {
     setTimeout(() => {
       const nativeElement = this.elementRef.nativeElement;
-      nativeElement.select();
+      nativeElement.setSelectionRange(0, nativeElement.value?.length ?? 0);
     }, 10);
   }
 }
