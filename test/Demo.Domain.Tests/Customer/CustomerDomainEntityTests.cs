f62117fecd593fa5c518fc974a0477946e10921c
Niels van der Vlies <nvlies@hotmail.com>
2022-06-06 09:44:57 +0200

    Added domain.tests project (wip)

---
+++ test/Demo.Domain.Tests/Customer/CustomerDomainEntityTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Demo.Domain.Customer.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Demo.Domain.Tests.Customer
+{
+    public class CustomerDomainEntityTests : DomainEntityTestsBase<Domain.Customer.Customer>
+    {
+        private readonly ICustomerDomainEntity _subject;
+
+        public CustomerDomainEntityTests()
+        {
+            _subject = ServiceProvider.GetService<ICustomerDomainEntity>();
+        }
+
+        [Fact]
+        public async Task CustomerDomainEntity_Create()
+        {
+            // Arrange
+
+            // Act
+            await _subject.NewAsync(CancellationToken.None);
+            await _subject.CreateAsync(CancellationToken.None);
+
+            // Assert
+        }
+    }
+}
---
+++ test/Demo.Domain.Tests/DomainEntityTestsBase.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Demo.Common;
+using Demo.Common.Helpers;
+using Demo.Common.Interfaces;
+using Demo.Domain.Shared.Interfaces;
+using Demo.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Microsoft.Extensions.Configuration;
+
+namespace Demo.Domain.Tests
+{
+    public abstract class DomainEntityTestsBase<T> where T: IEntity
+    {
+        protected ServiceProvider ServiceProvider;
+
+        protected readonly Mock<ICurrentUser> CurrentUserMock = new Mock<ICurrentUser>();
+        //protected readonly Mock<IDateTime> DateTimeMock = new Mock<IDateTime>();
+        //protected readonly Mock<IDbCommand<T>> DbCommandMock = new Mock<IDbCommand<T>>();
+
+        protected readonly Mock<IOutboxEventCreator> OutboxEventCreatorMock = new Mock<IOutboxEventCreator>();
+        protected readonly Mock<IOutboxMessageCreator> OutboxMessageCreatorMock = new Mock<IOutboxMessageCreator>();
+
+        protected DomainEntityTestsBase()
+        {
+            CurrentUserMock.Setup(x => x.Id).Returns(Guid.Parse("BCD06F09-25E6-4D2F-98CA-4A227902CDC4"));
+            //DateTimeMock.Setup(x => x.UtcNow).Returns(() => DateTime.UtcNow);
+
+            var myConfiguration = new Dictionary<string, string>
+            {
+                {"ConnectionStrings:SqlDatabase", "Data Source=localhost;initial catalog=Demo;Integrated Security=True; MultipleActiveResultSets=True"}
+            };
+
+            var configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(myConfiguration)
+                .Build();
+
+            var services = new ServiceCollection();
+            services.AddLogging();
+            services.AddCommon();
+            services.AddDomain();
+            services.AddInfrastructure(configuration);
+            services.AddSingleton(CurrentUserMock.Object);
+            //services.AddSingleton(DateTimeMock.Object);
+            //services.AddSingleton(DbCommandMock.Object);
+
+            //services.AddSingleton(OutboxEventCreatorMock.Object);
+            //services.AddSingleton(OutboxMessageCreatorMock.Object);
+            //services.AddSingleton(DbCommandMock.Object);
+            //services.AddSingleton(typeof(IJsonService<>), typeof(JsonService<>));
+            //services.AddTransient(typeof(IAuditlogger<>), typeof(Auditlogger<>));
+
+           // services.AddTransient(typeof(Lazy<>), typeof(LazyInstance<>));
+
+            /*AddInfra + replace
+             IEventPublisher
+IMessageSender
+ApplicationDbContext
+IDistributedMemoryCache
+IAuth0UserManagementClient
+IAuth0ManagementApiClientProvider
+             */
+            ServiceProvider = services.BuildServiceProvider();
+        }
+    }
+}
