a2392336d512d04ea3eeb0cb93694a677b43d546
Niels van der Vlies <nvlies@hotmail.com>
2022-06-18 11:36:35 +0200

    spinner styling

--- src/Demo.WebUI/src/app/shared/components/spinner/spinner.component.ts
+++ src/Demo.WebUI/src/app/shared/components/spinner/spinner.component.ts
@@ -1,4 +1,9 @@
-import { coerceNumberProperty, NumberInput } from '@angular/cdk/coercion';
+import {
+  BooleanInput,
+  coerceBooleanProperty,
+  coerceNumberProperty,
+  NumberInput
+} from '@angular/cdk/coercion';
 import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
 
 @Component({
@@ -15,6 +20,15 @@ export class SpinnerComponent implements OnInit {
   }
   set diameter(value: NumberInput) {
     this._diameter = coerceNumberProperty(value, 0);
+  }
+
+  private _fullHeight = false;
+  @Input()
+  get fullHeight() {
+    return this._fullHeight;
+  }
+  set fullHeight(value: BooleanInput) {
+    this._fullHeight = coerceBooleanProperty(value);
   }
 
   constructor() {}
