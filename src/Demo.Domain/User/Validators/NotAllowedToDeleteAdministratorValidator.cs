b9117379d289c5169b8ed713123c2c9c958bbc32
Niels van der Vlies <nvlies@hotmail.com>
2022-07-08 20:29:30 +0200

    prevent deletion of admin user

---
+++ src/Demo.Domain/User/Validators/NotAllowedToDeleteAdministratorValidator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Demo.Domain.Invoice;
+using Demo.Domain.Shared.DomainEntity;
+using Demo.Domain.Shared.Interfaces;
+using Demo.Domain.User.Seed;
+using Microsoft.EntityFrameworkCore;
+
+namespace Demo.Domain.User.Validators
+{
+    internal class NotAllowedToDeleteAdministratorValidator : IValidator<User>
+    {
+        public Task<IEnumerable<ValidationMessage>> ValidateAsync(IDomainEntityContext<User> context,
+            CancellationToken cancellationToken = default)
+        {
+            if (context.EditMode != EditMode.Delete)
+            {
+                return ValidationResultTask.Ok();
+            }
+
+            var isDefaultAdministrator = context.Entity.Id == DefaultAdministratorUser.UserId;
+
+            if (isDefaultAdministrator)
+            {
+                return ValidationResultTask.Invalid(
+                    "Cannot delete default administrator.");
+            }
+
+            return ValidationResultTask.Ok();
+        }
+    }
+}
---
+++ src/Demo.Domain/User/Validators/NotAllowedToDeleteSelfValidator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Demo.Domain.Shared.DomainEntity;
+using Demo.Domain.Shared.Interfaces;
+
+namespace Demo.Domain.User.Validators
+{
+    internal class NotAllowedToDeleteSelfValidator : IValidator<User>
+    {
+        private readonly ICurrentUserIdProvider _currentUserIdProvider;
+
+        public NotAllowedToDeleteSelfValidator(ICurrentUserIdProvider currentUserIdProvider)
+        {
+            _currentUserIdProvider = currentUserIdProvider;
+        }
+
+        public Task<IEnumerable<ValidationMessage>> ValidateAsync(IDomainEntityContext<User> context,
+            CancellationToken cancellationToken = default)
+        {
+            if (context.EditMode != EditMode.Delete)
+            {
+                return ValidationResultTask.Ok();
+            }
+
+            var isCurrentUser = context.Entity.Id == _currentUserIdProvider.Id;
+
+            if (isCurrentUser)
+            {
+                return ValidationResultTask.Invalid("Cannot delete own user.");
+            }
+
+            return ValidationResultTask.Ok();
+        }
+    }
+}
