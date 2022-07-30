f5fc0e78564b0e89a099edb2a4573e6a566de248
Niels van der Vlies <nvlies@hotmail.com>
2022-07-30 20:35:34 +0200

    updated nswag to .net6

--- tools/nswag/nswag.ps1
+++ tools/nswag/nswag.ps1
@@ -1,10 +1,10 @@
 $MyInvocation.MyCommand.Path | Split-Path | Push-Location
 
 Write-Host 'Installing Nswag...'
-npm i -g nswag@13.15.5
+npm i -g nswag@13.15.10
 
 Write-Host 'Generating api.generated.clients.ts...'
-nswag run nswag.json /runtime:Net50
+nswag run nswag.json /runtime:Net60
 
 @("// @ts-nocheck") + (get-content .\..\..\src\Demo.WebUI\src\app\api\api.generated.clients.ts) | set-content .\..\..\src\Demo.WebUI\src\app\api\api.generated.clients.ts
 
